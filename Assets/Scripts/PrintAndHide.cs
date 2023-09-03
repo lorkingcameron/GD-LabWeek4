d7e095f03f76034c0a460f92a5f983cb03133272
lorkingcameron <lorkingcameron@gmail.com>
2023-09-04 02:09:32 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     private int i;
+    private int randInt;
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        System.Random rnd = new System.Random();
+        randInt  = rnd.Next(150, 250);
     }
 
     // Update is called once per frame
@@ -17,5 +20,11 @@ public class ConsolePrint : MonoBehaviour
     {
         i++;
         Debug.Log(this.gameObject.name + ":" + i);
+        if (this.gameObject.tag == "Red" && i == 100) {
+            this.gameObject.SetActive(false);
+        }
+        if (this.gameObject.tag == "Blue" && i == randInt) {
+            this.gameObject.SetActive(false);
+        }
     }
 }
